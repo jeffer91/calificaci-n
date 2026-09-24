50e9223d9a4b677e7c38aae775c686a85f1a658a
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-24 16:35:17 -0500

    Add application configuration

---
+++ assets/config.js
@@ -0,0 +1,11 @@
+export const APP_CONFIG = {
+  apiBase: "",
+  firebase: {
+    apiKey: "AIzaSyCaHf1C0BB0X_H3BDZ1o-UDAsPmLTjsZLA",
+    authDomain: "utet-4387a.firebaseapp.com",
+    projectId: "utet-4387a",
+    storageBucket: "utet-4387a.firebasestorage.app",
+    messagingSenderId: "902848131454",
+    appId: "1:902848131454:web:47f515eb6480834724c32f"
+  }
+};
